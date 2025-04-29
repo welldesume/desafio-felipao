608b72e40e5d168602f3dbbdf80330d783a542f1
welldesume <wwelligtontrabalho@gmail.com>
2025-04-28 21:26:38 -0300

    meu setimo commit

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ console.log("meu terceiro commit");
 console.log("meuquarto commit")
 console.log("meu quinto commit");
 console.log("no sexto commit os comandos são: git add .  git commit -m ASPAS nome do commit ASPAS git pussh -u origin main ");
-
+console.log(" no setimo commit : obs. cria o repositorio e depois para unir a sua maquina é so seguir os passos que estao no git hub");
 
